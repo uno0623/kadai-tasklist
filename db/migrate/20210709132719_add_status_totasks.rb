8cb94f5e89f1889a5da64180b7e8122b3ca72524
satoshi.uno <taro.kirameki@techacademy.jp>
2021-07-09 13:54:48 +0000

    add column

---
+++ db/migrate/20210709132719_add_status_totasks.rb
@@ -0,0 +1,5 @@
+class AddStatusTotasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :status, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_151641) do
+ActiveRecord::Schema.define(version: 2021_07_09_132719) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "status"
   end
 
 end
